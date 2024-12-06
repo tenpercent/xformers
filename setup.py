d576b53f5df6fbf06e1f55a99f2343aace71a41f
Max Podkorytov <4273004+tenpercent@users.noreply.github.com>
2024-12-06 02:55:02 +0000

    do not offload compress prior to rocm 6.2

--- setup.py
+++ setup.py
@@ -463,13 +463,17 @@ def get_extensions():
 
         arch_list = os.getenv("HIP_ARCHITECTURES", "native").split()
 
+        offload_compress_flag = []
+        if "6.2." in hip_version:
+            offload_compress_flag = ["--offload-compress"]
+
         extra_compile_args = {
             "cxx": ["-O3", "-std=c++17"] + generator_flag,
             "nvcc": [
                 "-O3",
                 "-std=c++17",
                 *[f"--offload-arch={arch}" for arch in arch_list],
-                "--offload-compress",
+                *offload_compress_flag,
                 "-U__CUDA_NO_HALF_OPERATORS__",
                 "-U__CUDA_NO_HALF_CONVERSIONS__",
                 "-DCK_TILE_FMHA_FWD_FAST_EXP2=1",
